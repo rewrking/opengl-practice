e499e9e608ccdf54e9bb342d554a55d9a8ed1a83
Andrew King <pepsiotaku@gmail.com>
2023-03-12 13:45:33 -0400

    fix windows

--- src/Core/Platform.cpp
+++ src/Core/Platform.cpp
@@ -2,14 +2,14 @@
 
 #include "Core/OpenGL.hpp"
 
-#if defined(OG_WIN32)
+#if defined(OGL_WIN32)
 	#include "Core/Win32/WindowsPlatform.hpp"
 #endif
 
 /*****************************************************************************/
 void ogl::Platform::initialize(GLFWwindow* inWindow)
 {
-#if defined(OG_WIN32)
+#if defined(OGL_WIN32)
 	ogl::WindowsPlatform platform;
 	platform.initialize(inWindow);
 #else
